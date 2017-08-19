ad3c5298328291cc3f3ca4a96791b07101029373
Shadow <brianleon77@gmail.com>
2017-08-19 16:47:30 +0200

    Added dynamic menu

---
+++ menu.php
@@ -0,0 +1,114 @@
+ <!--
+        START: Navbar
+
+        Additional Classes:
+            .nk-navbar-sticky
+            .nk-navbar-transparent
+            .nk-navbar-autohide
+            .nk-navbar-light
+            .nk-navbar-no-link-effect
+    -->
+        <nav class="nk-navbar nk-navbar-top nk-navbar-sticky nk-navbar-transparent nk-navbar-autohide">
+            <div class="container">
+                <div class="nk-nav-table">
+
+                    <a href="index.php" class="nk-nav-logo">
+                        <img src="assets/images/logo.png" alt="" width="90">
+                    </a>
+
+
+                    <ul class="nk-nav nk-nav-right hidden-md-down" data-nav-mobile="#nk-nav-mobile">
+                        <li class="active  ">
+                            <a href="index.php">
+                Home</a>
+                        </li>
+                        <li class="  nk-drop-item">
+                            <a href=>
+                Account</a>
+                            <ul class="dropdown">
+                                <li class="  nk-drop-item">
+                                    <a href="">
+                World of Warcraft</a>
+                                    <ul class="dropdown">
+                                     <!--   <li class="  ">
+                                            <a href="layout-nav-default.php">
+                Vanilla (Soon?)</a>
+                                        </li>
+                                        <li class="  ">
+                                            <a href="layout-nav-default-transparent.php">
+                The Burning Crusade (Soon?)</a> -->
+                                        </li>
+                                        <li class="  ">
+                                            <a href="account/register/">
+                Wrath of the Lich King</a>
+                                        </li>
+                                                                           </ul>
+                                </li>
+                            <li class="  nk-drop-item">
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="">
+                            <a href="download.php">
+                Download</a>
+                            <div class="">
+                                    <ul>
+                                    <li>
+                                    </li>
+
+                            </div>
+                        </li>
+                        <li class="  nk-drop-item">
+                            <a href="">
+                Community</a>
+                            <ul class="dropdown">
+                                <li class="  ">
+                                    <a href="forum.php">
+                Forums</a>
+                                </li>
+                                <li class="  ">
+                                    <a href="https://discord.gg/5GDExP3">
+                Discord</a>
+                                </li>                              
+                            </ul>
+                        </li>
+                        <li class="">
+                            <a href="store.php">
+                Store</a>
+
+                        </li>
+                        <li class="  nk-drop-item">
+                            
+                            <ul class="dropdown">
+                                <li class="  ">
+                                    <a href="">
+                How to Connect</a>
+                                </li>
+                                <li class="  ">
+                                    <a href="">
+                FAQ</a>
+                                </li>
+                                <li class="  ">
+                                    <a href="">
+                Become a Developer!</a>
+                                </li>
+
+                                </li>
+
+                            </ul>
+                        </li>
+                        <li class="  ">
+                            <a href="bugtracker.php">
+                BugTracker</a>
+                        </li>
+                    </ul>
+
+                    <ul class="nk-nav nk-nav-right nk-nav-icons">
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- END: Navbar -->
+
+    </header>
