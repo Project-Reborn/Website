a0eb50e821dec32ce0b6b6f3ca83d8aa7ca04306
Shadow <brianleon77@gmail.com>
2017-08-19 17:01:19 +0200

    Added auth & characters database
    Want to see counters working? Insert this small .sql to see -- or use your own. :smile:

---
+++ auth&char-fordemo/auth&char.sql
@@ -0,0 +1,156 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.25-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for auth
+CREATE DATABASE IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `auth`;
+
+-- Dumping structure for table auth.account
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `sha_pass_hash` varchar(40) NOT NULL DEFAULT '',
+  `sessionkey` varchar(80) NOT NULL DEFAULT '',
+  `v` varchar(64) NOT NULL DEFAULT '',
+  `s` varchar(64) NOT NULL DEFAULT '',
+  `token_key` varchar(100) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `reg_mail` varchar(255) NOT NULL DEFAULT '',
+  `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_ip` varchar(15) NOT NULL DEFAULT '127.0.0.1',
+  `last_attempt_ip` varchar(15) NOT NULL DEFAULT '127.0.0.1',
+  `failed_logins` int(10) unsigned NOT NULL DEFAULT '0',
+  `locked` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `lock_country` varchar(2) NOT NULL DEFAULT '00',
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `expansion` tinyint(3) unsigned NOT NULL DEFAULT '2',
+  `mutetime` bigint(20) NOT NULL DEFAULT '0',
+  `mutereason` varchar(255) NOT NULL DEFAULT '',
+  `muteby` varchar(50) NOT NULL DEFAULT '',
+  `locale` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `os` varchar(3) NOT NULL DEFAULT '',
+  `recruiter` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='Account System';
+
+-- Dumping data for table auth.account: ~5 rows (approximately)
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` (`id`, `username`, `sha_pass_hash`, `sessionkey`, `v`, `s`, `token_key`, `email`, `reg_mail`, `joindate`, `last_ip`, `last_attempt_ip`, `failed_logins`, `locked`, `lock_country`, `last_login`, `online`, `expansion`, `mutetime`, `mutereason`, `muteby`, `locale`, `os`, `recruiter`) VALUES
+	(1, 'test1', '', '', '', '', '', '', '', '2017-08-18 07:43:29', '127.0.0.1', '127.0.0.1', 0, 0, '00', '0000-00-00 00:00:00', 0, 2, 0, '', '', 0, '', 0),
+	(2, 'test2', '', '', '', '', '', '', '', '2017-08-18 07:43:29', '127.0.0.1', '127.0.0.1', 0, 0, '00', '0000-00-00 00:00:00', 0, 2, 0, '', '', 0, '', 0),
+	(3, 'test3', '', '', '', '', '', '', '', '2017-08-19 16:58:09', '127.0.0.1', '127.0.0.1', 0, 0, '00', '0000-00-00 00:00:00', 0, 2, 0, '', '', 0, '', 0),
+	(4, 'test4', '', '', '', '', '', '', '', '2017-08-19 16:58:22', '127.0.0.1', '127.0.0.1', 0, 0, '00', '0000-00-00 00:00:00', 0, 2, 0, '', '', 0, '', 0),
+	(5, 'test5', '', '', '', '', '', '', '', '2017-08-18 07:43:29', '127.0.0.1', '127.0.0.1', 0, 0, '00', '0000-00-00 00:00:00', 0, 2, 0, '', '', 0, '', 0);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+
+
+-- Dumping database structure for characters
+CREATE DATABASE IF NOT EXISTS `characters` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `characters`;
+
+-- Dumping structure for table characters.characters
+CREATE TABLE IF NOT EXISTS `characters` (
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+  `account` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Account Identifier',
+  `name` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `race` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `class` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `gender` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `xp` int(10) unsigned NOT NULL DEFAULT '0',
+  `money` int(10) unsigned NOT NULL DEFAULT '0',
+  `skin` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `face` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `hairStyle` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `hairColor` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `facialStyle` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `bankSlots` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `restState` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `playerFlags` int(10) unsigned NOT NULL DEFAULT '0',
+  `position_x` float NOT NULL DEFAULT '0',
+  `position_y` float NOT NULL DEFAULT '0',
+  `position_z` float NOT NULL DEFAULT '0',
+  `map` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Map Identifier',
+  `instance_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `instance_mode_mask` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `orientation` float NOT NULL DEFAULT '0',
+  `taximask` text NOT NULL,
+  `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `cinematic` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `totaltime` int(10) unsigned NOT NULL DEFAULT '0',
+  `leveltime` int(10) unsigned NOT NULL DEFAULT '0',
+  `logout_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `is_logout_resting` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `rest_bonus` float NOT NULL DEFAULT '0',
+  `resettalents_cost` int(10) unsigned NOT NULL DEFAULT '0',
+  `resettalents_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `trans_x` float NOT NULL DEFAULT '0',
+  `trans_y` float NOT NULL DEFAULT '0',
+  `trans_z` float NOT NULL DEFAULT '0',
+  `trans_o` float NOT NULL DEFAULT '0',
+  `transguid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `extra_flags` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `stable_slots` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `at_login` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `zone` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `death_expire_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `taxi_path` text,
+  `arenaPoints` int(10) unsigned NOT NULL DEFAULT '0',
+  `totalHonorPoints` int(10) unsigned NOT NULL DEFAULT '0',
+  `todayHonorPoints` int(10) unsigned NOT NULL DEFAULT '0',
+  `yesterdayHonorPoints` int(10) unsigned NOT NULL DEFAULT '0',
+  `totalKills` int(10) unsigned NOT NULL DEFAULT '0',
+  `todayKills` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `yesterdayKills` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `chosenTitle` int(10) unsigned NOT NULL DEFAULT '0',
+  `knownCurrencies` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `watchedFaction` int(10) unsigned NOT NULL DEFAULT '0',
+  `drunk` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `health` int(10) unsigned NOT NULL DEFAULT '0',
+  `power1` int(10) unsigned NOT NULL DEFAULT '0',
+  `power2` int(10) unsigned NOT NULL DEFAULT '0',
+  `power3` int(10) unsigned NOT NULL DEFAULT '0',
+  `power4` int(10) unsigned NOT NULL DEFAULT '0',
+  `power5` int(10) unsigned NOT NULL DEFAULT '0',
+  `power6` int(10) unsigned NOT NULL DEFAULT '0',
+  `power7` int(10) unsigned NOT NULL DEFAULT '0',
+  `latency` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `talentGroupsCount` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `activeTalentGroup` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `exploredZones` longtext,
+  `equipmentCache` longtext,
+  `ammoId` int(10) unsigned NOT NULL DEFAULT '0',
+  `knownTitles` longtext,
+  `actionBars` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `grantableLevels` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `deleteInfos_Account` int(10) unsigned DEFAULT NULL,
+  `deleteInfos_Name` varchar(12) DEFAULT NULL,
+  `deleteDate` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`guid`),
+  KEY `idx_account` (`account`),
+  KEY `idx_online` (`online`),
+  KEY `idx_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Player System';
+
+-- Dumping data for table characters.characters: ~0 rows (approximately)
+/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
+INSERT INTO `characters` (`guid`, `account`, `name`, `race`, `class`, `gender`, `level`, `xp`, `money`, `skin`, `face`, `hairStyle`, `hairColor`, `facialStyle`, `bankSlots`, `restState`, `playerFlags`, `position_x`, `position_y`, `position_z`, `map`, `instance_id`, `instance_mode_mask`, `orientation`, `taximask`, `online`, `cinematic`, `totaltime`, `leveltime`, `logout_time`, `is_logout_resting`, `rest_bonus`, `resettalents_cost`, `resettalents_time`, `trans_x`, `trans_y`, `trans_z`, `trans_o`, `transguid`, `extra_flags`, `stable_slots`, `at_login`, `zone`, `death_expire_time`, `taxi_path`, `arenaPoints`, `totalHonorPoints`, `todayHonorPoints`, `yesterdayHonorPoints`, `totalKills`, `todayKills`, `yesterdayKills`, `chosenTitle`, `knownCurrencies`, `watchedFaction`, `drunk`, `health`, `power1`, `power2`, `power3`, `power4`, `power5`, `power6`, `power7`, `latency`, `talentGroupsCount`, `activeTalentGroup`, `exploredZones`, `equipmentCache`, `ammoId`, `knownTitles`, `actionBars`, `grantableLevels`, `deleteInfos_Account`, `deleteInfos_Name`, `deleteDate`) VALUES
+	(1, 2, 'Testxd', 7, 8, 1, 80, 0, 0, 4, 4, 5, 4, 6, 0, 2, 0, -6228.83, 329.624, 383.129, 0, 0, 0, 0.467313, '32 0 0 8 0 0 1048576 0 0 0 0 0 0 0 ', 1, 1, 172, 172, 1503151178, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 1, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 4294967295, 0, 7363, 6018, 0, 0, 100, 0, 0, 0, 14, 1, 0, '0 0 0 0 0 0 1048576 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ', '0 0 0 0 0 0 6096 0 56 0 0 0 1395 0 55 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 35 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ', 0, '0 0 0 0 0 0 ', 0, 0, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
